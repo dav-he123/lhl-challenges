d9a89810496b1e7b68ace60b4d6efddac8794f94
David <david.he07@yahoo.ca>
2020-04-10 17:34:01 -0400

    updated code for challenge 5

---
+++ js-basics-tutorial/challenge_5.js
@@ -0,0 +1,11 @@
+const slowRead = (fileNameOne, fileNameTwo, cb) => {
+  // code here...
+  console.log("Reading from file...");
+};
+
+slowRead("file1.txt", "file2.txt", (v) => console.log(`The value is ${v}`));
+// Sample Output
+// Reading Data from Files....
+// the value is 32
+// feel free to add more test cases. Also create files file1 and file2 to test
+this;
