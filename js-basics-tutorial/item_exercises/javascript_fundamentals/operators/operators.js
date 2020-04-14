178661c4d641aca47dca4949bfd3255d90156711
David <david.he07@yahoo.ca>
2020-04-14 17:52:28 -0400

    updated code for operators

---
+++ js-basics-tutorial/item_exercises/javascript_fundamentals/operators/operators.js
@@ -0,0 +1,25 @@
+// let a = 1,
+//   b = 1;
+
+// let c = ++a; // becomes c = 2
+// let d = b++; // remains same value of 1
+
+// let a = 2;
+
+// let x = 1 + (a *= 2); // x becomes the value of 5
+
+"" + 1 + 0; // 1
+"" - 1 + 0; // -1
+true + false; // 1
+6 / "3"; // 2
+"2" * "3"; // 6
+4 + 5 + "px"; //9
+"$" + 4 + 5; //$9
+"4" - 2; //2
+"4px" - 2; //NaN
+7 / 0; //infinity
+"  -9  " + 5; //-4
+"  -9  " - 5; //-14
+null + 1; //1
+undefined + 1; //NaN
+" \t \n" - 2; //-2
