866c6587147fca758fb72054a179ee526f23ddaf
David <david.he07@yahoo.ca>
2020-04-15 12:16:30 -0400

    updated code for switch_statement

--- js-basics-tutorial/item_exercises/javascript_fundamentals/if_statements/if_statements.js
+++ js-basics-tutorial/item_exercises/javascript_fundamentals/if_statements/if_statements.js
@@ -1,7 +1,10 @@
-let value = prompt("What is the value?", "");
+// let value = prompt("What is the value?", "");
 
-if (value >= 18) {
-  alert("You can access.");
-} else {
-  alert("You are not allowed access.");
-}
+// if (value >= 18) {
+//   alert("You can access.");
+// } else {
+//   alert("You are not allowed access.");
+// }
+
+let result;
+a + b < 4 ? (result = "Below") : (result = "Over");
