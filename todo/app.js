049309cbc24cce0b20dd67d0bd8b9742018391e3
David <david.he07@yahoo.ca>
2020-04-20 15:23:42 -0400

    updated code for app.js

--- todo/app.js
+++ todo/app.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ name: "user_id", secret: "asdfg" }));
+
+usersDB = {
+  FXF2X: { id: "FXF2X", email: "a@b.com", password: "password" },
+  D3XiL: { id: "D3XiL", email: "b@c.com", password: "password" },
+  //...
+};
 
 listsDB = {
   VbXn9: {
@@ -43,7 +53,7 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/todo.json", (req, res) => {
   res.json(listsDB);
 });
 
@@ -52,8 +62,11 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/todo", (req, res) => {
+  let user = req.session.user_id;
+
   let templateVars = {
     lists: listsDB,
+    user: usersDB[user],
   };
   res.render("index", templateVars);
 });
@@ -67,15 +80,153 @@ app.get("/todo/:uuid", (req, res) => {
     uuid: req.params.uuid,
     specificTask: listsDB[req.params.uuid],
     todoItems: listsDB[req.params.uuid].items,
+    user: usersDB[req.session.user_id],
   };
   res.render("show", templateVars);
+  return;
+});
+
+app.get("/register", (req, res) => {
+  let templateVars = {
+    user: undefined, //Need to make sure no logged in user can see this
+  };
+
+  res.render("registration", templateVars);
+});
+
+app.get("/login", (req, res) => {
+  let templateVars = {
+    user: undefined,
+  };
+
+  res.render("login", templateVars);
+});
+
+app.post("/todo/:uuid", (req, res) => {
+  listsDB[req.params.uuid].items.push(req.body.items),
+    // } else {
+    //   res.statusCode = 403;
+    //   res.send();
+    // }
+    res.redirect("/todo/" + uuid);
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res.statusCode = 403;
+    res.end("The email or password is empty. 403 Forbidden.");
+  } else {
+    const user = emailLookup(usersDB, email);
+    if (!user) {
+      res.statusCode = 403;
+      res.end("User is not found! Please try again!");
+    } else {
+      if (req.body.password === user.password) {
+        req.session.user_id = user.id;
+        res.redirect("/todo");
+      } else {
+        res.statusCode = 403;
+        res.end(
+          "Invalid password, please try again. The password is case sensitive."
+        );
+      }
+    }
+  }
+  getUserByEmail(req.body.email, usersDB);
+  res.cookie("name", "user_id");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
+  res.redirect("/todo");
+});
+
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res.statusCode = 400;
+    res.end("The email or password is empty. Status code: 400"); //This error message shows up if the user does not register email or password in the browser.
+  } else if (emailCheck(usersDB, req.body.email)) {
+    res.statusCode = 400;
+    res.end("The email is already being used. Status code: 400"); //This error message shows up when user registers the same username and password that has already been registered
+  }
+
+  let randomID = generateRandomString();
+
+  usersDB[randomID] = {
+    id: randomID,
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  // res.cookie("user_id", "ss@gmail.com");
+
+  req.session.user_id = randomID;
+  getUserByEmail(req.body.email, usersDB);
+
+  res.redirect("/todo");
 });
 
 app.post("/todo", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+
+  let uuid = generateRandomString();
+
+  listsDB[uuid] = {
+    name: req.body.name,
+  };
+
+  res.redirect("/todo/" + uuid);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let randString = Math.random().toString(36).substring(2, 7);
+  return randString;
+}
+
+const getUserByEmail = function (email, database) {
+  for (const key of Object.keys(database)) {
+    if (email === database[key].email) {
+      return database[key];
+    }
+  }
+  return false;
+};
+
+const emailCheck = function (usersDB, email) {
+  //The emailCheck function is used to compare the email in users object with email inputted by the user.
+  for (const check of Object.keys(usersDB)) {
+    if (email === usersDB[check].email) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const emailLookup = function (usersDB, email) {
+  // The emailLookup function is used to compare the email in users object with email inputted into the browser and returns email stored in object
+  for (const objectKey of Object.keys(usersDB)) {
+    if (email === usersDB[objectKey].email) {
+      console.log("true");
+      return usersDB[objectKey];
+    }
+  }
+  return false;
+};
+
+// function addingNewItem(addedItem) {
+//   const result = {};
+//   for (const obj in listsDB) {
+//     listsDB[obj].push(addedItem);
+//   }
+//   result = listsDB[obj];
+//   console.log(result);
+//   return result;
+// }
